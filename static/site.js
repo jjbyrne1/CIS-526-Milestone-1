70b50838d55ff9b956165c6a2f02ceb4ad2c8650
Jason Byrne <jjbyrne@ksu.edu>
2021-06-21 04:18:19 +0000

    Final commit for milestone 2

---
+++ static/site.js
@@ -0,0 +1,41 @@
+const path = "/project/static/box-locations.json";
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", path);
+xhr.send();
+
+xhr.addEventListener('load', ()=>{
+  displayCards(JSON.parse(xhr.responseText));
+});
+
+
+/** @function displayForecast 
+ * This function updates the div with id "card-container" on the 
+ * page to display the forcast contained in the data argument.
+ * @param {object} data - the forecast object from api.weather.gov 
+ */
+function displayCards(data){
+  //var current = data.properties.periods[0];
+  
+  var display = document.getElementById("cardContainer");
+  display.textContent = "";
+  
+  data.forEach((boxLocation) => {
+    var card = document.createElement("div");
+    display.appendChild(card);
+    
+    var desc = document.createElement("h3");
+    desc.textContent = boxLocation.name;
+    
+    var img = document.createElement("img");
+    img.src = "https://open.mapquestapi.com/staticmap/v4/getmap?key=9V9esHFJ0JNWGiuIXVieiLhBHzseqFTD&size=600,400&zoom=16&center="
+         + boxLocation.lat + "," + boxLocation.lng;
+    img.alt="img";
+    
+    
+    card.appendChild(desc);
+    card.appendChild(img);
+    
+  });             
+  
+
